de23c24dc046a70ca31d74b0f9b31aa06b24378e
Aishe <aishe3819@gmail.com>
2020-05-28 11:41:29 +0300

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await axios.get(`${state.apiUrl}`, {
+                    params: {
+                        q: plan,
+                        app_id: 'fe1df224',
+                        app_key: 'd0df9d7c22e80e1a021fae427687dbed',
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        }
+    }
 });
